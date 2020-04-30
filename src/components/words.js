da63bbf71bbde909a1a7005ca758c13c8f82e6bb
sabrina2712 <mailroshni@gmail.com>
2020-04-30 11:13:33 -0400

    add error message

--- src/components/words.js
+++ src/components/words.js
@@ -4,29 +4,57 @@ export default class Words extends Component {
   constructor(props) {
     super(props);
     this.words = [];
-    this.state = { word: "" };
+    this.state = { word: "", showWords: this.words, errorMessage: "" };
   }
 
   addWords = (word) => {
-    this.words.push(word);
+    if (word.length < 1) {
+      return null;
+    } else this.words.push(word);
   };
   onTextChangeHandler = (event) => {
+    let text = event.target.value;
     this.setState((state) => {
-      state.word = event.target.value;
+      state.word = text;
       return state;
     });
   };
   onShowMeHandler = () => {
-    this.setState({});
+    if (this.words.length < 3) {
+      this.setState({ errorMessage: "please enter three" });
+    }
   };
-  onAddHandler = (e) => {};
+  onAddHandler = (e) => {
+    this.addWords(this.state.word);
+    this.setState({ word: "" });
+  };
+
+  errorMessage = () => {
+    if (!this.state.errorMessage) {
+      return null;
+    }
+    return <div>{this.state.errorMessage}</div>;
+  };
   render() {
     return (
       <div>
-        <input type="text" name="word"></input>
+        <input
+          id="inputText"
+          type="text"
+          name="word"
+          value={this.state.word}
+          onChange={this.onTextChangeHandler}
+        />
+
+        {this.errorMessage()}
         <br />
-        <button onClick={this.onShowMeHandler}> Show mw the message</button>
-        <button onClick={this.onAddHandler}>Add the word</button>
+        <button onClick={this.onShowMeHandler}> Show me the message</button>
+        <button
+          disabled={this.state.word.length < 1}
+          onClick={this.onAddHandler}
+        >
+          Add Words
+        </button>
       </div>
     );
   }
